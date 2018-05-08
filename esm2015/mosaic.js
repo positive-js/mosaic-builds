797c93872b7617199939babb7cebc4908744189e
Oleg Pimenov <pimenov.o@gmail.com>
2018-05-08 19:38:42 +0000

    master - 01818cd added codecov

--- esm2015/mosaic.js
+++ esm2015/mosaic.js
@@ -11,7 +11,7 @@ export { McButtonModule, McButtonCSSStyler, McXSButtonCSSStyler, McSMButtonCSSSt
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
  */
-const /** @type {?} */ VERSION = new Version('0.0.1-4aadf66');
+const /** @type {?} */ VERSION = new Version('0.0.1-01818cd');
 
 /**
  * @fileoverview added by tsickle
--- esm5/mosaic.es5.js
+++ esm5/mosaic.es5.js
@@ -11,7 +11,7 @@ export { McButtonModule, McButtonCSSStyler, McXSButtonCSSStyler, McSMButtonCSSSt
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
  */
-var /** @type {?} */ VERSION = new Version('0.0.1-4aadf66');
+var /** @type {?} */ VERSION = new Version('0.0.1-01818cd');
 
 /**
  * @fileoverview added by tsickle
