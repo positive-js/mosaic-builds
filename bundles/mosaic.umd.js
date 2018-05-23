2000c7fd5ba882b90016e0b8b1b2d8634d118a52
lkramarov <lkramarov@ptsecurity.com>
2018-05-23 15:14:53 +0000

    master - a55ced4 feat(typography): Typography (#144638)

--- bundles/mosaic.umd.js
+++ bundles/mosaic.umd.js
@@ -572,7 +572,7 @@ var McButtonModule = /** @class */ (function () {
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
  */
-var /** @type {?} */ VERSION = new core.Version('0.0.1-293766c');
+var /** @type {?} */ VERSION = new core.Version('0.0.1-a55ced4');
 
 exports.VERSION = VERSION;
 exports.McButtonModule = McButtonModule;
--- esm5/mosaic.es5.js
+++ esm5/mosaic.es5.js
@@ -11,7 +11,7 @@ export { McButtonModule, McButtonCSSStyler, McXSButtonCSSStyler, McSMButtonCSSSt
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
  */
-var /** @type {?} */ VERSION = new Version('0.0.1-293766c');
+var /** @type {?} */ VERSION = new Version('0.0.1-a55ced4');
 
 /**
  * @fileoverview added by tsickle
