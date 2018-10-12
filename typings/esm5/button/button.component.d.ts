e7f54d00dee493e06d5b41da70690ab85411fab1
Oleg Pimenov <pimenov.o@gmail.com>
2018-10-12 13:55:08 +0000

    master - 92fcee8 fix(button): prevent events for disable button

--- typings/esm5/button/button.component.d.ts
+++ typings/esm5/button/button.component.d.ts
@@ -1,6 +1,5 @@
 import { ElementRef, OnDestroy, QueryList } from '@angular/core';
 import { FocusMonitor } from '@ptsecurity/cdk/a11y';
-import { Platform } from '@ptsecurity/cdk/platform';
 import { CanColor, CanDisable, CanDisableCtor, CanColorCtor } from '@ptsecurity/mosaic/core';
 import { McIcon } from '@ptsecurity/mosaic/icon';
 export declare class McButtonCSSStyler {
@@ -26,18 +25,17 @@ export declare class McButtonBase {
 }
 export declare const _McButtonMixinBase: CanDisableCtor & CanColorCtor & typeof McButtonBase;
 export declare class McButton extends _McButtonMixinBase implements OnDestroy, CanDisable, CanColor {
-    private _platform;
     private _focusMonitor;
-    constructor(elementRef: ElementRef, _platform: Platform, _focusMonitor: FocusMonitor);
+    constructor(elementRef: ElementRef, _focusMonitor: FocusMonitor);
     ngOnDestroy(): void;
     focus(): void;
     _getHostElement(): any;
 }
 export declare class McAnchor extends McButton {
-    constructor(platform: Platform, focusMonitor: FocusMonitor, elementRef: ElementRef);
+    constructor(focusMonitor: FocusMonitor, elementRef: ElementRef);
     _haltDisabledEvents(event: Event): void;
 }
 export declare class McIconButton extends McButton {
-    constructor(platform: Platform, focusMonitor: FocusMonitor, elementRef: ElementRef);
+    constructor(focusMonitor: FocusMonitor, elementRef: ElementRef);
     _haltDisabledEvents(event: Event): void;
 }
