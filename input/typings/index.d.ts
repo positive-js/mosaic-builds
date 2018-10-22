883654cf73bc2106b4cb8a3eee885f3669d417a1
Oleg Pimenov <pimenov.o@gmail.com>
2018-10-22 21:44:14 +0000

    master - c3cd645 chore: updated scrollable

--- input/typings/index.d.ts
+++ input/typings/index.d.ts
@@ -2,5 +2,5 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { MAX_VALIDATOR as ɵd19, MIN_VALIDATOR as ɵb19, MaxValidator as ɵe19, MinValidator as ɵc19 } from './input-number-validators';
-export { MC_INPUT_VALUE_ACCESSOR as ɵa19 } from './input-value-accessor';
+export { MAX_VALIDATOR as ɵd18, MIN_VALIDATOR as ɵb18, MaxValidator as ɵe18, MinValidator as ɵc18 } from './input-number-validators';
+export { MC_INPUT_VALUE_ACCESSOR as ɵa18 } from './input-value-accessor';
