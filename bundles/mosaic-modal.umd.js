a5dd34bfb28021343c0662c0bdc27bcc5a853a34
Oleg Pimenov <pimenov.o@gmail.com>
2018-12-13 21:13:46 +0000

    master - 627740e chore: updated npm deps

--- bundles/mosaic-modal.umd.js
+++ bundles/mosaic-modal.umd.js
@@ -876,8 +876,8 @@ var McModalModule = /** @class */ (function () {
     return McModalModule;
 }());
 
-exports.ɵb23 = CssUnitPipe;
-exports.ɵa23 = McModalControlService;
+exports.ɵb24 = CssUnitPipe;
+exports.ɵa24 = McModalControlService;
 exports.McModalComponent = McModalComponent;
 exports.McModalRef = McModalRef;
 exports.McModalModule = McModalModule;
