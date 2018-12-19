c39f0e6d72e384dc7a5a55cfaced4b52c9962e0d
Oleg Ulyanov <imrekbg@gmail.com>
2018-12-19 08:05:36 +0000

    master - a618795 chore: plugin tslint-language-service replaced typescript-tslint-plugin (#78)

--- typings/esm5/input/index.d.ts
+++ typings/esm5/input/index.d.ts
@@ -2,5 +2,5 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { MAX_VALIDATOR as ɵd22, MIN_VALIDATOR as ɵb22, MaxValidator as ɵe22, MinValidator as ɵc22 } from './input-number-validators';
-export { MC_INPUT_VALUE_ACCESSOR as ɵa22 } from './input-value-accessor';
+export { MAX_VALIDATOR as ɵd23, MIN_VALIDATOR as ɵb23, MaxValidator as ɵe23, MinValidator as ɵc23 } from './input-number-validators';
+export { MC_INPUT_VALUE_ACCESSOR as ɵa23 } from './input-value-accessor';
