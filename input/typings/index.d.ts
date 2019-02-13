1231b64953cc9407120590d739457233873e1da3
Oleg Pimenov <pimenov.o@gmail.com>
2019-02-13 19:56:07 +0000

    master - bce62ac chore: updated docs to yarn

--- input/typings/index.d.ts
+++ input/typings/index.d.ts
@@ -2,4 +2,4 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { MAX_VALIDATOR as ɵc23, MIN_VALIDATOR as ɵa23, MaxValidator as ɵd23, MinValidator as ɵb23 } from './input-number-validators';
+export { MAX_VALIDATOR as ɵc22, MIN_VALIDATOR as ɵa22, MaxValidator as ɵd22, MinValidator as ɵb22 } from './input-number-validators';
