02d78f8f98e014efb88a06030a83191da68a10c9
Oleg Pimenov <pimenov.o@gmail.com>
2019-04-06 14:44:50 +0000

    master - 409f291 chore: added collective to package json

--- typings/modal/index.d.ts
+++ typings/modal/index.d.ts
@@ -2,5 +2,5 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { CssUnitPipe as ɵb26 } from './css-unit.pipe';
-export { McModalControlService as ɵa26 } from './modal-control.service';
+export { CssUnitPipe as ɵb25 } from './css-unit.pipe';
+export { McModalControlService as ɵa25 } from './modal-control.service';
