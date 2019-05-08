a8e82a219ebf3035385e5eef5b46cb7d752169a7
Oleg Pimenov <pimenov.o@gmail.com>
2019-05-08 08:05:22 +0000

    master - 8eee6e5 Revert "chore: updated vendor package gulp-markdown to 4.0.0 (#107)"

--- esm2015/modal.js
+++ esm2015/modal.js
@@ -1009,5 +1009,5 @@ McModalModule.decorators = [
  * @suppress {checkTypes,extraRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 
-export { McModalComponent, McModalRef, McModalModule, McModalService, CssUnitPipe as ɵb25, McModalControlService as ɵa25 };
+export { McModalComponent, McModalRef, McModalModule, McModalService, CssUnitPipe as ɵb27, McModalControlService as ɵa27 };
 //# sourceMappingURL=modal.js.map
--- modal/typings/index.d.ts
+++ modal/typings/index.d.ts
@@ -2,5 +2,5 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { CssUnitPipe as ɵb25 } from './css-unit.pipe';
-export { McModalControlService as ɵa25 } from './modal-control.service';
+export { CssUnitPipe as ɵb27 } from './css-unit.pipe';
+export { McModalControlService as ɵa27 } from './modal-control.service';
