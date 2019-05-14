3ba1e5774492157cddebbe58b32a1f2867c4880b
Oleg Pimenov <pimenov.o@gmail.com>
2019-05-14 08:21:58 +0000

    master - f508b8d fix(build): docker image

--- esm2015/modal.js
+++ esm2015/modal.js
@@ -1009,5 +1009,5 @@ McModalModule.decorators = [
  * @suppress {checkTypes,extraRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 
-export { McModalComponent, McModalRef, McModalModule, McModalService, CssUnitPipe as ɵb26, McModalControlService as ɵa26 };
+export { McModalComponent, McModalRef, McModalModule, McModalService, CssUnitPipe as ɵb27, McModalControlService as ɵa27 };
 //# sourceMappingURL=modal.js.map
