b627221877b2ab6844e710afffabfa6a3987440e
Leon Vay <leon.vay@gmail.com>
2019-05-27 12:58:15 +0000

    master - 37c2782 fix(popover): Popover missing public api export (#128)

--- esm5/input.es5.js
+++ esm5/input.es5.js
@@ -958,5 +958,5 @@ var McInputModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 
-export { McInputModule, BIG_STEP, SMALL_STEP, McInputBase, _McInputMixinBase, McNumberInput, McInput, McInputMono, stepUp, stepDown, MC_INPUT_VALUE_ACCESSOR, MAX_VALIDATOR as ɵc24, MIN_VALIDATOR as ɵa24, MaxValidator as ɵd24, MinValidator as ɵb24 };
+export { McInputModule, BIG_STEP, SMALL_STEP, McInputBase, _McInputMixinBase, McNumberInput, McInput, McInputMono, stepUp, stepDown, MC_INPUT_VALUE_ACCESSOR, MAX_VALIDATOR as ɵc25, MIN_VALIDATOR as ɵa25, MaxValidator as ɵd25, MinValidator as ɵb25 };
 //# sourceMappingURL=input.es5.js.map
