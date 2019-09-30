b8d71944ac1137f56120640cfedd8aeb5f16a6a0
Oleg Pimenov <pimenov.o@gmail.com>
2019-09-30 20:46:49 +0000

    master - e2c7598 Update CHANGELOG.md

--- select/typings/select.component.d.ts
+++ select/typings/select.component.d.ts
@@ -146,7 +146,7 @@ export declare class McSelect extends McSelectMixinBase implements AfterContentI
     /** Whether the select is focused. */
     /**
     * @deprecated Setter to be removed as this property is intended to be readonly.
-    * @breaking-change 8.0.0-a0e0f65
+    * @breaking-change 8.0.0-e2c7598
     */
     focused: boolean;
     private _focused;
