8acbe26022cdca49b02eefbf82d20edb66bcb729
Oleg Ulyanov <oulyanov@ptsecurity.com>
2019-10-23 11:19:14 +0000

    master - 4cd8a2e chore: notification about release (#296)

--- typings/esm5/sidepanel/index.d.ts
+++ typings/esm5/sidepanel/index.d.ts
@@ -2,5 +2,5 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { mcSidepanelAnimations as ɵb20, mcSidepanelTransformAnimation as ɵa20 } from './sidepanel-animations';
-export { McSidepanelActions as ɵg20, McSidepanelBody as ɵe20, McSidepanelClose as ɵc20, McSidepanelFooter as ɵf20, McSidepanelHeader as ɵd20 } from './sidepanel-directives';
+export { mcSidepanelAnimations as ɵb21, mcSidepanelTransformAnimation as ɵa21 } from './sidepanel-animations';
+export { McSidepanelActions as ɵg21, McSidepanelBody as ɵe21, McSidepanelClose as ɵc21, McSidepanelFooter as ɵf21, McSidepanelHeader as ɵd21 } from './sidepanel-directives';
