addeed74e5f181a7e37c1fce84b96e43e1447ca4
Oleg Pimenov <pimenov.o@gmail.com>
2020-05-18 10:08:20 +0000

    master - aacb65e chore: updated yarn patches

--- schematics/ng-update/data/input-names.js
+++ schematics/ng-update/data/input-names.js
@@ -1,4 +1,34 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.inputNames = {};
+const schematics_1 = require("@angular/cdk/schematics");
+exports.inputNames = {
+    [schematics_1.TargetVersion.V9]: [
+        {
+            pr: '',
+            changes: [
+                {
+                    replace: 'min-time',
+                    replaceWith: 'min',
+                    whitelist: {
+                        attributes: ['mcTimepicker']
+                    }
+                },
+                {
+                    replace: 'max-time',
+                    replaceWith: 'max',
+                    whitelist: {
+                        attributes: ['mcTimepicker']
+                    }
+                },
+                {
+                    replace: 'time-format',
+                    replaceWith: 'format',
+                    whitelist: {
+                        attributes: ['mcTimepicker']
+                    }
+                }
+            ]
+        }
+    ]
+};
 //# sourceMappingURL=input-names.js.map
